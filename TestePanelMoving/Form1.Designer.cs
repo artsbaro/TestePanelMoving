4f4cf414e6f81ac7bd7a4b6d809588e537d53027
Antonio Raulande <artsbaro@hotmail.com>
2021-03-23 10:31:03 -0300

    Add project files.

---
+++ TestePanelMoving/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace TestePanelMoving
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelAtivarLabel1 = new System.Windows.Forms.Label();
+            this.labelAtivarLabel2 = new System.Windows.Forms.Label();
+            this.labelAtivarLabel3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(251, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(215, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(251, 54);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(215, 100);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(251, 54);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(215, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(44, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(44, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(26, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // labelAtivarLabel1
+            // 
+            this.labelAtivarLabel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelAtivarLabel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAtivarLabel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelAtivarLabel1.Location = new System.Drawing.Point(251, 34);
+            this.labelAtivarLabel1.Name = "labelAtivarLabel1";
+            this.labelAtivarLabel1.Size = new System.Drawing.Size(70, 17);
+            this.labelAtivarLabel1.TabIndex = 4;
+            this.labelAtivarLabel1.Text = "AtivarLabel1";
+            this.labelAtivarLabel1.Click += new System.EventHandler(this.AtivarLabel1_Click);
+            // 
+            // labelAtivarLabel2
+            // 
+            this.labelAtivarLabel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelAtivarLabel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAtivarLabel2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelAtivarLabel2.Location = new System.Drawing.Point(324, 34);
+            this.labelAtivarLabel2.Name = "labelAtivarLabel2";
+            this.labelAtivarLabel2.Size = new System.Drawing.Size(70, 17);
+            this.labelAtivarLabel2.TabIndex = 5;
+            this.labelAtivarLabel2.Text = "AtivarLabel2";
+            this.labelAtivarLabel2.Click += new System.EventHandler(this.AtivarLabel2_Click);
+            // 
+            // labelAtivarLabel3
+            // 
+            this.labelAtivarLabel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelAtivarLabel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAtivarLabel3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelAtivarLabel3.Location = new System.Drawing.Point(396, 34);
+            this.labelAtivarLabel3.Name = "labelAtivarLabel3";
+            this.labelAtivarLabel3.Size = new System.Drawing.Size(70, 17);
+            this.labelAtivarLabel3.TabIndex = 6;
+            this.labelAtivarLabel3.Text = "AtivarLabel3";
+            this.labelAtivarLabel3.Click += new System.EventHandler(this.AtivarLabel3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(92, 71);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Hab 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(92, 95);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(55, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Hab 2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(92, 118);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(55, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Hab 3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 276);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.labelAtivarLabel3);
+            this.Controls.Add(this.labelAtivarLabel2);
+            this.Controls.Add(this.labelAtivarLabel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        protected System.Windows.Forms.Label label1;
+        protected System.Windows.Forms.Label label2;
+        protected System.Windows.Forms.Label label3;
+        protected System.Windows.Forms.Label labelAtivarLabel1;
+        protected System.Windows.Forms.Label labelAtivarLabel2;
+        protected System.Windows.Forms.Label labelAtivarLabel3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ TestePanelMoving/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestePanelMoving
+{
+    public partial class Form1 : Form
+    {
+        private short LabelWidth = 70;
+        private short LabelSpace = 3;
+
+        private static Point LabelLocationStart = new Point(251, 34);
+
+        public Form1()
+        {
+            InitializeComponent();
+            AtivarPanel1();
+            CheckAllBoxes();
+        }
+
+        private void CheckAllBoxes()
+        {
+            checkBox1.Checked = true;
+            checkBox2.Checked = true;
+            checkBox3.Checked = true;
+        }
+
+        private void AtivarLabel1_Click(object sender, EventArgs e)
+        {
+            AtivarPanel1();
+        }
+
+        private void AtivarPanel1()
+        {
+            panel1.Visible = true;
+            DesativarPanel2();
+            DesativarPanel3();
+        }
+        private void AtivarPanel2()
+        {
+            panel2.Visible = true;
+            DesativarPanel1();
+            DesativarPanel3();
+        }
+        private void AtivarPanel3()
+        {
+            panel3.Visible = true;
+            DesativarPanel2();
+            DesativarPanel1();
+        }
+        private void DesativarPanel1()
+        {
+            panel1.Visible = false;
+        }
+        private void DesativarPanel2()
+        {
+            panel2.Visible = false;
+        }
+        private void DesativarPanel3()
+        {
+            panel3.Visible = false;
+        }
+
+        private void AtivarLabel2_Click(object sender, EventArgs e)
+        {
+            AtivarPanel2();
+        }
+
+        private void AtivarLabel3_Click(object sender, EventArgs e)
+        {
+            AtivarPanel3();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelAtivarLabel1.Visible = checkBox1.Checked;
+            panel1.Visible = checkBox1.Checked;
+            PosicionaLabels();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            labelAtivarLabel2.Visible = checkBox2.Checked;
+            panel2.Visible = checkBox2.Checked;
+            PosicionaLabels();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            labelAtivarLabel3.Visible = checkBox3.Checked;
+            panel3.Visible = checkBox3.Checked;
+            PosicionaLabels();
+        }
+
+        private void PosicionaLabels()
+        {
+            Point nextLabelLocation = LabelLocationStart;
+            bool labelSelected = false;
+            if (checkBox1.Checked)
+            {
+                labelAtivarLabel1.Location = LabelLocationStart;
+                nextLabelLocation = new Point(
+                    LabelLocationStart.X + LabelWidth + LabelSpace,
+                    LabelLocationStart.Y);
+
+                labelSelected = true;
+                AtivarLabel1_Click(this, null);
+            }
+
+            if (checkBox2.Checked)
+            {
+                labelAtivarLabel2.Location = nextLabelLocation;
+
+                nextLabelLocation = new Point(
+                    nextLabelLocation.X + LabelWidth + LabelSpace,
+                    nextLabelLocation.Y);
+
+                if (!labelSelected)
+                    AtivarLabel2_Click(this, null);
+
+                labelSelected = true;
+            }
+
+            if (checkBox3.Checked)
+            {
+                labelAtivarLabel3.Location = nextLabelLocation;
+
+                if (!labelSelected)
+                    AtivarLabel3_Click(this, null);
+            }
+        }
+    }
+}
